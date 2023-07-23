f4888517c5e30adf56395bfe3372cadcdb6846b5
Phil <>
2023-07-23 13:42:12 +0100

    Added functions to the Dictionary class which returns the list of words which begin with defined starting letters

--- Dictionary.java
+++ Dictionary.java
@@ -4,7 +4,8 @@ import java.io.*;
 import java.util.*;
 
 class Dictionary {
-    
+
+    // Constructor to the class which loads in the whole dictionary and reuces it to the set of valid words for the problem
     Dictionary(int n, String letters) {
 
         try {
@@ -39,18 +40,36 @@ class Dictionary {
                     }
                 // Print the string if it matches the criteria
                 if(charInWord){
-                    System.out.println(st);
+                    m_dict.add(st);
                 }
                 }
             }
         }
         catch (FileNotFoundException e) {
+            System.out.println("Dictionary enable1.txt not found, please download to the folder");
         }
         catch (IOException e) {
+            System.out.println("problem reading the file");
         }
     }
-  
 
-    private Vector<String> m_dict;
+    // Function to incriment the starting letters of the word
+    private static String incrementString(final String startingLetters) {
+        final StringBuilder sb = new StringBuilder(startingLetters);
+
+        // Increment the last character and return the result:
+        sb.setCharAt(sb.length() - 1, (char) (sb.charAt(sb.length() - 1) + 1));
+        return sb.toString();
+    }
+    
+    // Function to return the list of valid words given a set of starting letters
+    SortedSet<String> getValidWords(String startingWith){
+    final String endpoint = incrementString(startingWith);
+
+        return m_dict.subSet(startingWith, endpoint);
+    }
+  
+    // Member variables
+    private TreeSet<String> m_dict = new TreeSet<String>();
     private int m_noLetters;
 }
--- Solver.java
+++ Solver.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 class Solver {
     public static void main(String[] args) {
-        Dictionary d = new Dictionary(2, "abc"); 
+        Dictionary d = new Dictionary(2, "abc");
+        SortedSet<String> attempt = d.getValidWords("c");
+
+        System.out.println(attempt.size());
+        for (String t : attempt) {
+            System.out.println(t);
+        }
     }
 }
